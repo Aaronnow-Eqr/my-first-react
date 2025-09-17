286994432146e695d11b477a2cb2922b84722d35
Aaronnow-Eqr <79377629+Aaronnow-Eqr@users.noreply.github.com>
2025-09-17 18:31:42 -0400

    mi boton

---
+++ React/my-first-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Boton from './components/boton'
+
+function App() {
+  return (
+    <section>
+      <h1>mira mi boton</h1>
+      <Boton />
+      <p>hecho con react</p>
+    </section>
+  )
+}
+
+export default App;
---
+++ React/my-first-react/src/components/boton.jsx
@@ -0,0 +1,12 @@
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Boton() {
+  return (
+    <div>
+      <button>mi boton</button>
+    </div>
+  )
+}
+
+export default Boton;
