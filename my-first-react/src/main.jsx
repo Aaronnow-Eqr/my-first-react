983bf85acc0f3fea03a9dac8d2209acfdf2ae627
Aaronnow-Eqr <79377629+Aaronnow-Eqr@users.noreply.github.com>
2025-10-01 19:02:59 -0400

    .

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
-//import App from './App.jsx'
+import './index.css'
+import App from './App.jsx'
 import { Home } from './Pages/Home.jsx'
 import { Button } from './components/UI/Button.jsx'
 import { Anchor } from './components/UI/Anchor.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { State } from './Pages/State.jsx'
 import { Practica } from './Pages/Practica.jsx'
+import { Effect } from './Pages/Effect.jsx'
+import { Character } from './Pages/Character.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -18,7 +20,9 @@ createRoot(document.getElementById('root')).render(
         <Route path="/contact" element={<p>Contact</p>} />
         <Route path="/state" element={<State />} />
         <Route path="/practica" element={<Practica />} />
+        <Route path="/Effect" element={<Effect/>} />
+        <Route path="/character/:id" element={<Character/>} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
